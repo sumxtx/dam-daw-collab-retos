047a6c4e6de0beb2c24f7ec596c2d3bd523d6f19
Rober <robert1993_sira@hotmail.com>
2025-04-11 16:10:08 +0200

    #000 PYTHON

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RobertoIzquierdoGomez.py
@@ -0,0 +1,22 @@
+
+# Documentacion de python https://docs.python.org/es/3.9/c-api/intro.html
+
+# Comentario de una línea
+
+# Comentarios
+# de 
+# varias lineas
+
+"""
+Este comentario se utiliza para documentación, sobre todo de funciones.
+"""
+
+# Python es un lengua de debilmente tipado, es decir, no es necesario declarar el tipo de variable
+
+numero = 1
+caracter = 'a'
+string = "Hola mundo"
+booleano = True
+sinAsignar = None
+
+print("Hola Python")
