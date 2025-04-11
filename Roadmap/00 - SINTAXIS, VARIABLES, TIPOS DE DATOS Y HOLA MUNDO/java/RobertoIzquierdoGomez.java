a99ccd52891f4d33f224a7cc3c8c955133cb8811
Rober <robert1993_sira@hotmail.com>
2025-04-11 15:51:42 +0200

    #000 - JAVA

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/RobertoIzquierdoGomez.java
@@ -0,0 +1,27 @@
+public class RobertoIzquierdoGomez{
+
+    /**
+         * Comentario para documentación
+         * @author etiquetas para documentacion
+         * @version 1.0
+        */
+
+    public static void main(String[] args) {
+        //Web JAVA https://docs.oracle.com/javase/8/docs/api/
+        //Comentario de una línea
+        /* Comentario para varias lineas */
+        byte enteroPequeno = 1;
+        byte hexadecimal = 0x00;
+        int numeroEntero = 100;
+        long numeroEnteroGrande = 999999999;
+        char caracter = 'a';
+        String cadenaTexto = "Hola mundo"; //Esta variable no es primitiva, pero es importante conocer y diferenciar char de String.
+        boolean booleano = true;
+
+        System.out.println("Hola JAVA");
+
+
+
+    }
+
+}
